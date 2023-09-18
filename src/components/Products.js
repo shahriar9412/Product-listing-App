05f90fc31a922e03ccba7f4c4e24b9f773125ca4
shahriar-hossain <shahriar9412@gmail.com>
2023-09-18 16:08:39 +0600

    first commit

---
+++ src/components/Products.js
@@ -0,0 +1,18 @@
+/* eslint-disable react/prop-types */
+import React from 'react';
+import Product from './Product.js';
+
+export default function Products ( {products})
+{
+    return(
+    <div className="products">
+    {
+        products.map
+        (
+        ( product ) =>(<Product key={ product.id } product={ product }/>)
+        )
+    }
+    </div>
+    );
+
+};
